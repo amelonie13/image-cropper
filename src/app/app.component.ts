a031e43fdb7c4150134c72eb811ca317ff79f616
ameliemuller <amelie@startup-werk.de>
2024-07-25 17:51:21 +0200

    make app component stand alone

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,11 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule, MatButtonModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
