421fecda0bc83a3f0312300d8768deb9d8a01502
ameliemuller <amelie@startup-werk.de>
2024-07-25 17:52:45 +0200

    add missing config and routes

---
+++ src/app/app.config.ts
@@ -0,0 +1,8 @@
+import { ApplicationConfig, importProvidersFrom } from "@angular/core";
+import { provideRouter } from '@angular/router';
+import { appRoutes } from "./app.routes";
+
+export const appConfig: ApplicationConfig = {
+providers: [provideRouter(appRoutes), importProvidersFrom()]
+
+}
