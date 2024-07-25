ce42d29257b9c522d2a68dab0e0cc1d1eb14d08c
ameliemuller <amelie@startup-werk.de>
2024-07-25 23:05:40 +0200

    using clip and cdk drag and drop v.1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
+import { ImageCropperComponent } from './image-cropper/image-cropper.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, MatButtonModule],
+  imports: [CommonModule, MatButtonModule, ImageCropperComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
--- src/app/image-cropper/image-cropper.component.ts
+++ src/app/image-cropper/image-cropper.component.ts
@@ -1,15 +1,75 @@
 import { Component, OnInit } from '@angular/core';
+import {CdkDrag} from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-image-cropper',
+  standalone: true,
+  imports: [CdkDrag],
   templateUrl: './image-cropper.component.html',
   styleUrls: ['./image-cropper.component.scss']
 })
 export class ImageCropperComponent implements OnInit {
+  dragPosition = {x: 0, y: 0};
+
 
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  getImage(event: any) {
+    const canvas = document.getElementById("imgCanvas") as HTMLCanvasElement;
+    const target = event.target as HTMLInputElement;
+    if (!target) {
+      return;
+    }
+    const file: any = target.files?.[0];
+    const image = new Image();
+    image.src = URL.createObjectURL(file);
+    image.onload = () => {
+      this.showImage(image, canvas);
+    }
+  }
+
+  showImage(img: any, canvas: any){
+    canvas.width = img.naturalWidth;
+    canvas.height = img.naturalHeight;
+    var context = canvas.getContext("2d");
+    if(context)
+    {
+      context.drawImage(img, 0, 0);
+    }
+    else
+    {
+      alert("Error: Context not defined!");
+    }
+  }
+ pixel (x: any,y:any)
+{
+	const canvas = document.getElementById('imgCanvas');
+    const h = canvas!.offsetHeight;
+    const w = canvas!.offsetWidth;
+    const out = (y*w+x)*4;
+
+    return(out);
+}
+cropImage(event?: any){
+  const image = new Image();
+  let canvas = document.getElementById('imgCanvas') as HTMLCanvasElement;
+  let output = document.getElementById('output') as HTMLCanvasElement;
+  image.src = canvas.toDataURL();
+  output.width = image.width;
+  output.height = image.height;
+  let firstCtx = canvas.getContext('2d')!
+  let secondCtx = output.getContext('2d')!
+  let x = event.dropPoint.x
+  let y = event.dropPoint.y
+
+  // Create a circular clipping path
+  secondCtx.beginPath();
+  secondCtx.rect(x -100 , y -100, 200, 200);
+  secondCtx.clip();
+  secondCtx!.drawImage(image, 0,0 )
+}
 
 }
