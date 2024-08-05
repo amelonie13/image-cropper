2563870f26c28c12bbad11d396ae78a0ace7020f
ameliemuller <amelie@startup-werk.de>
2024-08-05 12:17:17 +0200

    make prettier

--- src/app/image-cropper/image-cropper.component.ts
+++ src/app/image-cropper/image-cropper.component.ts
@@ -1,24 +1,22 @@
 import { Component, OnInit } from '@angular/core';
-import {CdkDrag} from '@angular/cdk/drag-drop';
+import { CdkDrag } from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-image-cropper',
   standalone: true,
   imports: [CdkDrag],
   templateUrl: './image-cropper.component.html',
-  styleUrls: ['./image-cropper.component.scss']
+  styleUrls: ['./image-cropper.component.scss'],
 })
 export class ImageCropperComponent implements OnInit {
-  dragPosition = {x: 0, y: 0};
+  dragPosition = { x: 0, y: 0 };
 
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   getImage(event: any) {
-    const canvas = document.getElementById("imgCanvas") as HTMLCanvasElement;
+    const canvas = document.getElementById('imgCanvas') as HTMLCanvasElement;
     const target = event.target as HTMLInputElement;
     if (!target) {
       return;
@@ -28,65 +26,77 @@ export class ImageCropperComponent implements OnInit {
     image.src = URL.createObjectURL(file);
     image.onload = () => {
       this.showImage(image, canvas);
-    }
+    };
   }
 
-  showImage(img: any, canvas: any){
+  showImage(img: any, canvas: any) {
     canvas.width = img.naturalWidth;
     canvas.height = img.naturalHeight;
-    var context = canvas.getContext("2d");
-    if(context)
-    {
+    var context = canvas.getContext('2d');
+    if (context) {
       context.drawImage(img, 0, 0);
-    }
-    else
-    {
-      alert("Error: Context not defined!");
+    } else {
+      alert('Error: Context not defined!');
     }
   }
- pixel (x: any,y:any)
-{
-	const canvas = document.getElementById('imgCanvas');
+  pixel(x: any, y: any) {
+    const canvas = document.getElementById('imgCanvas');
     const h = canvas!.offsetHeight;
     const w = canvas!.offsetWidth;
-    const out = (y*w+x)*4;
+    const out = (y * w + x) * 4;
 
-    return(out);
-}
-cropImage(event?: any){
-  const image = new Image();
-  let canvas = document.getElementById('imgCanvas') as HTMLCanvasElement;
-  let output = document.getElementById('output') as HTMLCanvasElement;
-
-  image.src = canvas.toDataURL();
-  output.width = image.width;
-  output.height = image.height;
-  let firstCtx = canvas.getContext('2d')!
-  let secondCtx = output.getContext('2d')!
-  let x = event.dropPoint.x
-  let y = event.dropPoint.y
-  let finalWidth = output.width 
-  const width = this.calculatAspectRatioW(image.width, canvas.getBoundingClientRect().width, 200);
-  const height = this.calculatAspectRatioH(image.height, canvas.getBoundingClientRect().height, 200);
-
-  secondCtx.beginPath();
-  secondCtx.rect(x , y, width, height);
-  secondCtx.clip();
-  secondCtx!.drawImage(image, 0,0 )
-  let dataURL = output.toDataURL("image/png");
-}
-calculatAspectRatioW(originalImgWidth: any, currentW: any,  cropperWidth: any){
-  if(originalImgWidth !== 0){
-  return  originalImgWidth / currentW * cropperWidth;
-  } else {
-    return 200;
+    return out;
   }
-}
-calculatAspectRatioH(originalImgHeigth: any, currentH: any,  cropperHeigth: any){
-  if(originalImgHeigth !== 0){
-  return  originalImgHeigth /currentH * cropperHeigth;
-  } else {
-    return 200;
+  cropImage(event?: any) {
+    const image = new Image();
+    let canvas = document.getElementById('imgCanvas') as HTMLCanvasElement;
+    let output = document.getElementById('output') as HTMLCanvasElement;
+
+    image.src = canvas.toDataURL();
+    output.width = image.width;
+    output.height = image.height;
+    let firstCtx = canvas.getContext('2d')!;
+    let secondCtx = output.getContext('2d')!;
+    let x = event.dropPoint.x;
+    let y = event.dropPoint.y;
+    let finalWidth = output.width;
+    const width = this.calculatAspectRatioW(
+      image.width,
+      canvas.getBoundingClientRect().width,
+      200
+    );
+    const height = this.calculatAspectRatioH(
+      image.height,
+      canvas.getBoundingClientRect().height,
+      200
+    );
+
+    secondCtx.beginPath();
+    secondCtx.rect(x, y, width, height);
+    secondCtx.clip();
+    secondCtx!.drawImage(image, 0, 0);
+    let dataURL = output.toDataURL('image/png');
   }
-}
+  calculatAspectRatioW(
+    originalImgWidth: any,
+    currentW: any,
+    cropperWidth: any
+  ) {
+    if (originalImgWidth !== 0) {
+      return (originalImgWidth / currentW) * cropperWidth;
+    } else {
+      return 200;
+    }
+  }
+  calculatAspectRatioH(
+    originalImgHeigth: any,
+    currentH: any,
+    cropperHeigth: any
+  ) {
+    if (originalImgHeigth !== 0) {
+      return (originalImgHeigth / currentH) * cropperHeigth;
+    } else {
+      return 200;
+    }
+  }
 }
